3bd5a7ca229c26841434478100f812ef90a59d65
darlenenyambura <darlene.nyambura@student.moringaschool.com>
2025-03-10 11:45:40 +0300

    updating scores and switching turns

--- src/Scrabblescore.js
+++ src/Scrabblescore.js
@@ -4,3 +4,28 @@ import {getLetterScore} from "./Scrabbleboard"
 //Score calculation
 //scores
 
+
+const useScrabbleScore = () => {
+    const [playeerScores, setPlayerScores] = useState();
+    const [currentPlayer, setCurrentPlayer] = useState();
+};
+
+
+//updating scores
+const updateScore = () => {
+    const wordScore = calculateScore();
+    setPlayerScores((prevScores) => ({
+        ...prevScores, [currentPlayer] : prevScores[currentPlayer] + wordScore,
+    }));
+    switchTurn();
+};
+
+//switching turns
+const switchTurn = () => {
+    setCurrentPlayer((prev) => (
+        prev === "player1" ? "player2" : "player1"
+    ));
+};
+
+
+
